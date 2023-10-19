06c0194bc5391407e208746764e17fc966b50d2e
turzacse <turzacse@gmail.com>
2023-10-19 22:59:38 +0600

    Validate the user when he/she register/login to the website and set the error message

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,52 +1,85 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { GoogleAuthProvider, createUserWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
 import { FcGoogle } from 'react-icons/fc';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import auth from '../../Firebase/firebase.config';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AuthContext } from '../../Provider/AuthProvider';
+import Swal from 'sweetalert2';
 
 const Login = () => {
 
-    const {signIn} = useContext(AuthContext);
+    const { signIn } = useContext(AuthContext);
     const location = useLocation();
     const navigate = useNavigate();
+    const [error, setError] = useState('');
 
 
-    const handleLogin = e =>{
+    const handleLogin = e => {
         e.preventDefault();
+
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password);
 
+        setError('');
         // user create 
         signIn(email, password)
-        .then(result => {
-            console.log(result.user);
+            .then(result => {
+                console.log(result.user);
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: 'Log in Successful',
+                    showConfirmButton: false,
+                    timer: 3000
+                })
+                navigate(location?.state ? location.state : '/');
+            })
+            .catch(error => {
+                setError('Invalid email or password. Please try again.');
+                console.error(error);
+            })
+    }
 
-            navigate(location?.state ? location.state : '/');
-        })
-        .catch(error =>{
-            console.error(error);
-        })
+    const provider = new GoogleAuthProvider();
+    const googleSignIn = e => {
+        setError('');
+        e.preventDefault();
+        signInWithPopup(auth, provider)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+
+                navigate(location?.state ? location.state : '/');
+            })
+            .catch(error => {
+                console.error(error);
+                setError(error.message);
+            })
     }
     return (
         <div className="w-1/3 mx-auto bg-base-400 shadow-2xl p-10">
             <form onSubmit={handleLogin}>
-            <input type="email" name="email" placeholder="Your Email" className="input input-bordered w-full" required />
-            <input type="password" name="password" placeholder="Password" className="input input-bordered w-full my-4" required/>
+                <input type="email" name="email" placeholder="Your Email" className="input input-bordered w-full" required />
+                <input type="password" name="password" placeholder="Password" className="input input-bordered w-full my-4" required />
 
-            <input type="submit" className="btn w-full bg-success" value="Login" />
-            <button className='text-xl text-center mt-5'>
-            <FcGoogle className='text-center text-3xl'></FcGoogle>
-            </button>
-            <p>Not Registered?? 
-            <Link 
-            className='text-blue-400 ml-2'
-            to='/register'
-            >Please Register</Link> </p>
+                <input type="submit" className="btn w-full bg-success" value="Login" />
+                {
+                    error && <p className="text-red-400">{error}</p>
+                }
+                <button
+                    onClick={googleSignIn}
+                    className='text-xl text-center mt-5'>
+                    <FcGoogle className='text-center text-3xl'></FcGoogle>
+                </button>
+                <p>Not Registered??
+                    <Link
+                        className='text-blue-400 ml-2'
+                        to='/register'
+                    >Please Register</Link> </p>
 
 
-        </form>
+            </form>
         </div>
     );
 };
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,49 +1,92 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { Link } from "react-router-dom";
-import auth from "../../Firebase/firebase.config";
-import { useContext } from "react";
+
+import { Link, useNavigate } from "react-router-dom";
+import { useContext, useState } from "react";
 import { AuthContext } from "../../Provider/AuthProvider";
+import Swal from "sweetalert2";
 
 const Register = () => {
 
-    const {createUser} = useContext(AuthContext);
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
+
+    const navigate = useNavigate();
+
+    const { createUser } = useContext(AuthContext);
 
     const handleRegister = e => {
         e.preventDefault();
         const email = e.target.email.value;
         const password = e.target.password.value;
-        console.log(email, password);
+        const photo = e.target.photo.value;
+        const name = e.target.name.value;
+
+        console.log(email, password, name, photo);
+
+        const capitalLetter = /[A-Z]/;
+        const specialCharacter = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/;
+
+        setError('');
+        setSuccess('');
+        if (password.length < 6) {
+            setError('Password should be 6 charecter or longer');
+            return;
+        }
+        if (!capitalLetter.test(password)) {
+            setError('Password sould contain at least one Capital letter');
+            return;
+        }
+
+        if (!specialCharacter.test(password)) {
+            setError('Password sould contain at least one Special Character');
+            return;
+        }
 
         // user create 
         createUser(email, password)
-        .then(result => {
-            console.log(result.user);
-        })
-        .catch(error =>{
-            console.error(error);
-        })
+            .then(result => {
+                console.log(result.user);
+                setSuccess('User Created Successfully');
+                Swal.fire({
+                    position: 'center',
+                    icon: 'success',
+                    title: 'Registration Successful',
+                    showConfirmButton: false,
+                    timer: 3000
+                })
+                navigate('/');
+            })
+            .catch(error => {
+                console.error(error);
+                setError(error.message);
+            })
     }
     return (
         <div>
             <div className="w-1/3 mx-auto bg-base-400 shadow-2xl p-10">
-            <form onSubmit={handleRegister}>
-            <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered w-full" required />
-            <input type="text" name="name" placeholder="Your Name" className="input input-bordered w-full my-4" required />
-            <input type="email" name="email" placeholder="Your Email" className="input input-bordered w-full" required />
-            <input type="password" name="password" placeholder="Password" className="input input-bordered w-full my-4" required/>
+                <form onSubmit={handleRegister}>
+                    <input type="text" name="photo" placeholder="Photo URL" className="input input-bordered w-full" required />
+                    <input type="text" name="name" placeholder="Your Name" className="input input-bordered w-full my-4" required />
+                    <input type="email" name="email" placeholder="Your Email" className="input input-bordered w-full" required />
+                    <input type="password" name="password" placeholder="Password" className="input input-bordered w-full my-4" required />
 
-            <input type="submit" className="btn w-full bg-success" value="Login" />
-            <button className='text-xl text-center mt-5'>
-            </button>
-            <p>Already Registered?? 
-            <Link 
-            className='text-blue-400 ml-2'
-            to='/login'
-            >Please Login</Link> </p>
+                    <input type="submit" className="btn w-full bg-success" value="Login" />
+                    <button className='text-xl text-center mt-5'>
+                    </button>
+                    {
+                        error && <p className="text-red-400">{error}</p>
+                    }
+                    {
+                        success && <p>{success}</p>
+                    }
+                    <p>Already Registered??
+                        <Link
+                            className='text-blue-400 ml-2'
+                            to='/login'
+                        >Please Login</Link> </p>
 
 
-        </form>
-        </div>
+                </form>
+            </div>
         </div>
     );
 };
